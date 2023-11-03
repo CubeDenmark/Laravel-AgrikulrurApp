d25e156e7964f16246680445b4977ba01804b964
Teddy Pascual <teddylayson2000@gmail.com>
2023-11-03 21:55:12 +0800

    login and register page add icon logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Login</title>
     <link rel="stylesheet" href="../css/Login.css" />
+    <link rel="shortcut icon" href="../assets/favicon.ico" type="image/x-icon"> 
     <!-- Boostrap CSS -->
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
