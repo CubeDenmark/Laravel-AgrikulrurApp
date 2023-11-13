8ae1c9a4a3dde8f450e6bc63eea40a77c85cd99f
Teddy Pascual <teddylayson2000@gmail.com>
2023-11-14 00:31:32 +0800

    added demand auctions nav buttons

--- resources/views/demandAuctionListings.blade.php
+++ resources/views/demandAuctionListings.blade.php
@@ -31,12 +31,49 @@
       href="https://fonts.googleapis.com/css2?family=Merriweather&display=swap"
       rel="stylesheet"
     />
-    
+    {{-- Help Button --}}
+    <button class="btn btn-success help-btn" data-bs-toggle="modal" data-bs-target="#auctionModal"><i class="fa-solid fa-circle-info help-txt"></i></button>
+    {{-- Help Button --}}
+
+    {{-- Help  Modal --}}
+    <div class="modal fade" id="demandModal" tabindex="-1" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title md-title text-success" id="demandModalLabel">Demand Auctions Page Guide</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+          <iframe width="100%" height="100%" src="https://www.youtube.com/embed/GkGxeuew2ac?si=JaPuwV-StTRh0mOZ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary fs-2" data-bs-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+{{-- Help  Modal --}}
     <p class="title text-center mt-5 mb-5">
-      Welcome to Demand Auctions Page fafaf</span>
+      Welcome to Demand Auctions Page</span>
     </p>
-    <div class="container-fluid d-flex justify-content-between">
+    <div class="d-flex justify-content-between">
       <span></span>
+      <div class="d-flex">
+            <a
+              href="{{url('crop-options')}}"
+              class="md-title text-start mx-4 text-decoration-none text-dark"
+              >Supply</a
+            >
+            <p class="md-title text-start mx-4">|</p>
+            <a
+              href="{{ url('demandAuctions') }}"
+              class="md-title text-start mx-4 text-decoration-none text-success"
+              >Demand</a
+            >
+        </div>
+    </div>
+    <div class="container-fluid d-flex justify-content-between">
+      
       @if(Auth::user()->type == "bidder")
       <a
         href="{{ url('create_demAuction')}}"
